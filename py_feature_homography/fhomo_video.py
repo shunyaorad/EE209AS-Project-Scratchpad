5f8decf01a431702c4155c46be748b70f6fda440
shun <shunyaorad@gmail.com>
2016-10-26 01:16:32 -0700

    added comments, fixed zero division error

--- py_feature_homography/fhomo_video.py
+++ py_feature_homography/fhomo_video.py
@@ -3,12 +3,23 @@ import cv2
   
 ESC=27   
 camera = cv2.VideoCapture(0)
+
+# The parameter cv2.NORM_HAMMING specifies the distance measurement to be used, 
+# in this case, hamming distance. For ORB this is the only option we have for this parameter.
+# The second parameter is a boolean  if it is true, the matcher returns only those matches 
+# with value (i,j) such that i-th descriptor in set A has j-th descriptor in set B as the 
+# best match and vice-versa.
 orb = cv2.ORB_create()
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
 
 imgTrainColor=cv2.imread('images/tag_4.jpg')
 imgTrainGray = cv2.cvtColor(imgTrainColor, cv2.COLOR_BGR2GRAY)
 
+# the first function is used to get the Key points in the image that we pass 
+# as first argument, the second argument is a mask but here we will use none. 
+# the second function computes the descriptors of the image using the keypoints. 
+# this image shows the key points obtained by ORB
 kpTrain = orb.detect(imgTrainGray,None)
 kpTrain, desTrain = orb.compute(imgTrainGray, kpTrain)
 
@@ -18,12 +29,19 @@ while True:
    
     ret, imgCamColor = camera.read()
     imgCamGray = cv2.cvtColor(imgCamColor, cv2.COLOR_BGR2GRAY)
+
+    #  the keypoints and compute the descriptors as before. 
     kpCam = orb.detect(imgCamGray,None)
     kpCam, desCam = orb.compute(imgCamGray, kpCam)
+
+    # we need to delete the matches that correspond to errors, 
+    # we do this by setting a distance threshold and obtain the matches points 
+    # whose distances are less than the threshold. Here I used as threshold half 
+    # of the distance mean across all the array of matching points. 
     matches = bf.match(desCam,desTrain)
     dist = [m.distance for m in matches]
 
-    # Prevent zero division error
+    # Prevent zero division error. Need to modify so that camera dont drop frame
     if len(dist) == 0:
         continue
 
@@ -42,10 +60,22 @@ while True:
     result[hdif:hdif+h2, :w2] = imgTrainColor
     result[:h1, w2:w1+w2] = imgCamColor
 
+    # for every matching point we get the point in both images 
+    #a draw a line between them. prior to this we must create the 
+    #result image that is going to have both matching images. 
     for i in range(len(matches)):
+        # points on train image
         pt_a=(int(kpTrain[matches[i].trainIdx].pt[0]), int(kpTrain[matches[i].trainIdx].pt[1]+hdif))
+
+        # points on camera
         pt_b=(int(kpCam[matches[i].queryIdx].pt[0]+w2), int(kpCam[matches[i].queryIdx].pt[1]))
+
         cv2.line(result, pt_a, pt_b, (255, 0, 0))
+
+    if len(matches) > 3:
+        print "found!"
+    else:
+        print "No detection!"
 
     cv2.imshow('Camara', result)
   
